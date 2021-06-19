1f1d837821d8fe114e6b45720da7337a893f7540
cruzgerman216 <cruzgerman216@yahoo.com>
2021-06-19 12:05:15 -0500

    GC: users controller updated

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :first_name, :last_name, :username, :email, :password, :city, :state, :birth_year, :birth_day, :about_me, :created_at, :updated_at
-json.url user_url(user, format: :json)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+x = 0
+while x < 10
+    num_paragraphs = Random.new.rand(0...10)
+    random = Random.new.rand(0...9)
+
+    u = User.create(
+        username: Faker::Internet.username,
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name ,
+        email: Faker::Internet.email,
+        password: "password",
+        city: "Van Buren",
+        state: "Arkansas",
+        birth_year: 1995,
+        birth_day: 26,
+        about_me: Faker::Lorem.paragraphs(number:num_paragraphs).join(" ")
+    )
+    x+=1
+end
