f33d2816015523b504f4f03aa965fcad0cca21f7
cruzgerman216 <cruzgerman216@yahoo.com>
2021-06-19 23:30:44 -0500

    GC: Passing user's username tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, uniqueness:true 
+    validates :username, presence: true, length: {minimum: 2, maximum: 15}, uniqueness:true 
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "email format" do
-    user = User.new(email: "test@test.com")
-    assert_equal(user.save, true, "user is saved")
+  test "user can be saved" do
+    user = User.new(username: "username", email: "test@test.com")
+    assert_equal(user.save, true, "user can be saved with the appropriate keys")
   end
 
-  test "space in front and back of email results to false" do
+  test "user cannot save without email" do
+    user = User.new
+    assert_equal(user.save, false, "cannot save without email")
+  end
+
+  test "an emsil with space in front and back of email results to false" do
     user = User.new(email: " test@test.com ")
     assert_equal(user.save, false, "user is saved")
   end
 
-  test "Without an @ is false" do
+  test "Without an '@' is false" do
     user = User.new(email: "testtest.com")
     assert_equal(user.save, false, "user is saved")
   end
@@ -21,4 +26,20 @@ class UserTest < ActiveSupport::TestCase
     assert_equal(user.save, false, "user is saved")
   end
 
+  test "Username can't be less than 2" do
+    user = User.new(username: "a",email:"test@test.com")
+    assert_equal(user.save, false, "username cannot have less than 2 characters")
+  end
+
+  test "Username can't be more than 10" do
+    user = User.new(username: "12345678910111213",email:"test@test.com")
+    assert_equal(user.save, false, "username cannot be more than 10 characters")
+  end
+
+  test "Username can't be a duplicate" do
+    user = User.new(username: "12345678910111213",email:"test@test.com")
+    user.save 
+    user = User.new(username: "12345678910111213",email:"test@test.com")
+    assert_equal(user.save, false, "username cannot be duplicate")
+  end
 end
