f8a045f747322dc577f2ac7220feb5ee14b0ba03
cruzgerman216 <cruzgerman216@yahoo.com>
2021-06-22 11:13:18 -0500

    GC: validating and testing passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,28 @@ class User < ApplicationRecord
     validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, uniqueness:true 
     validates :username, presence: true, length: {minimum: 2, maximum: 15}, uniqueness:true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
     validate :username_needs_at_least_one_letter 
-
+    validates :first_name, :last_name, presence: true, length: {minimum: 1, maximum: 30}, format: { with: /\A[a-zA-Z]+\Z/ }
+    validates :password, presence: true, length: {minimum: 5, maximum: 30}
+    validate :bad_password
+    
     def username_needs_at_least_one_letter 
         if username && username.count("a-zA-Z") == 0
             errors.add(:username, "at least one letter")
         end
     end
+
+    def bad_password
+        if password 
+            if password.count("a-z") == 0
+                errors.add(:password, "at least one lowercase letter")
+            elsif password.count("A-Z") == 0
+                errors.add(:password, "at least one upper case letter")
+            elsif password.count("0-9") == 0
+                errors.add(:password, "at least one number")
+            elsif password.count("!@#$%\\^&*()_\\-+=") == 0
+                errors.add(:password, "at least one symbol")
+            end
+        end
+    end
+    
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "user can be saved" do
-    user = User.new(username: "username", email: "test@test.com")
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"Password123!")
     assert_equal(user.save, true, "user can be saved with the appropriate keys")
   end
 
@@ -12,57 +12,101 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "an email with space in front and back of email results to false" do
-    user = User.new(username: "username", email: " test@test.com ")
+    user = User.new(username: "username", email: " test@test.com ", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "user is saved")
   end
 
   test "Without an '@' is false" do
-    user = User.new(username: "username", email: "testtest.com")
+    user = User.new(username: "username", email: "testtest.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "user is saved")
   end
 
   test "Without a '.com' is false" do
-    user = User.new(username: "username",email: "test@test")
+    user = User.new(username: "username",email: "test@test", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "user is saved")
   end
 
   test "Username can't be less than 2" do
-    user = User.new(username: "a",email:"test@test.com")
+    user = User.new(username: "a",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username cannot have less than 2 characters")
   end
 
   test "Username can't be more than 10" do
-    user = User.new(username: "usernameusername",email:"test@test.com")
+    user = User.new(username: "usernameusername",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username cannot be more than 10 characters")
   end
 
   test "Username can't be a duplicate" do
-    user = User.new(username: "username",email:"test@test.com")
+    user = User.new(username: "username",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     user.save 
-    user = User.new(username: "username",email:"test@test.com")
+    user = User.new(username: "username",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username cannot be duplicate")
   end
 
   test "Username cannot be spaces" do
-    user = User.new(username: " rtrt  ",email:"test@test.com")
+    user = User.new(username: " rtrt  ",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username cannot be spaces")
   end
 
   test "Username cannot be an empty string" do
-    user = User.new(username: "",email:"test@test.com")
+    user = User.new(username: "",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username cannot be empty string ever")
   end
 
 
   test "Username cannot have any special characters" do
-    user = User.new(username: "user!@#",email:"test@test.com")
+    user = User.new(username: "user!@#",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username cannot have any special characters")
   end
 
   test "should not save username without at least one letter" do
-    user = User.new(username: "123",email:"test@test.com")
-
+    user = User.new(username: "123",email:"test@test.com", first_name: "first", last_name:"Last", password:"password123!")
     assert_equal(user.save, false, "username needs at least one letter")
   end
 
+  test "should first names and last names not contain any numbers" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "fi2rst", last_name:"La2st", password:"password123!")
+    assert_equal(user.save, false, "first names and last names cannot have numbers")
+  end
+
+  test "should first names and last names not contain any symbols" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "fi%rst", last_name:"La$st", password:"password123!")
+    assert_equal(user.save, false, "first names and last names cannot have numbers")
+  end
+
+  test "should require a password from user" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last")
+    assert_equal(user.save, false, "passwords are required when saving a user")
+  end
+
+
+  test "should password not contain less than 5 characters" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"a1!")
+    assert_equal(user.save, false, "passwords cannot be less than 5 characters or more than 30 characters")
+  end
+
+  test "should not contain a password that is more than 30 characters" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"a1!12123451234567891012345678910")
+    assert_equal(user.save, false, "passwords cannot be less than 5 characters or more than 30 characters")
+  end
+
+  test "should not contain a password that is missing a lower case letter" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"A12345!")
+    assert_equal(user.save, false, "passwords require at least one lowercase letter")
+  end
+
+  test "should not contain a password that is missing a upper case letter" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"a12345!")
+    assert_equal(user.save, false, "passwords require at least one upper case letter")
+  end
+
+  test "should not contain a password that is misssing a number" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"Abcdef!")
+    assert_equal(user.save, false, "passwords require at least one number")
+  end
+
+  test "should not contain a password that is misssing a symbol" do
+    user = User.new(username: "username", email: "test@test.com", first_name: "first", last_name:"Last", password:"Abcdef123")
+    assert_equal(user.save, false, "passwords require at least one symbol")
+  end
 end
